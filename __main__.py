cbccf4c6103a53590f1d9b517c1df6e0731c7e5c
kyledunne <kyledunne@berkeley.edu>
2017-12-21 21:29:47 -0500

    Most functionality now in place.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,12 @@
 import os
 from tkinter import *
+from tkinter import filedialog
 import integration
 
 image_dir = os.path.join('res', 'img')
+watched_folders = integration.get_watched_folders()
+managed_files = integration.get_file_list()
+default_download_destination = integration.get_default_download_destination()
 
 
 def upload():
@@ -14,7 +18,59 @@ def upload_all():
 
 
 def open_manage_watched_folders_window():
-    pass
+    ManageWatchedFoldersWindow()
+
+
+class ManageWatchedFoldersWindow:
+
+    def __init__(self):
+        window = Toplevel()
+        window.title('Manage Watched Folders')
+        self.listbox_frame = Frame(window)
+        self.listbox_frame.pack(side=LEFT, fill=Y)
+        self.current_inspected_folder = None
+        self.scrollbar = Scrollbar(self.listbox_frame)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.folders_listbox = Listbox(self.listbox_frame, yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.folders_listbox.yview)
+        self.folders_listbox.pack(side=LEFT)
+        width = 20
+        for folder in watched_folders:
+            width = max(width, len(folder))
+            self.folders_listbox.insert(END, folder)
+        self.folders_listbox.config(width=min(50, width+3))
+        self.folders_listbox.bind('<ButtonRelease-1>', self.find_and_inspect_folder)
+
+        self.right_side_frame = Frame(window)
+        self.right_side_frame.pack(side=RIGHT, fill=Y)
+        self.add_button = Button(self.right_side_frame, text='Add...', command=self.add)
+        self.add_button.pack(side=TOP)
+        self.remove_button = Button(self.right_side_frame, text='Stop Watching', command=self.remove)
+        self.remove_button.pack(side=BOTTOM)
+        self.remove_button.config(state=DISABLED)
+        self.inspected_folder_name_label = Label(self.right_side_frame, text='')
+        self.inspected_folder_name_label.pack(side=BOTTOM)
+        Label(self.right_side_frame, text='--------------------').pack(side=BOTTOM)
+
+    def find_and_inspect_folder(self, event_data):
+        self.current_inspected_folder = self.folders_listbox.get(self.folders_listbox.curselection()[0])
+        self.inspected_folder_name_label.config(text=self.current_inspected_folder)
+        self.remove_button.config(state=NORMAL)
+
+    def add(self):
+        folder_to_add = filedialog.askdirectory()
+        if folder_to_add:
+            integration.watch(folder_to_add)
+            watched_folders.append(folder_to_add)
+            self.folders_listbox.insert(END, folder_to_add)
+
+    def remove(self):
+        integration.remove_from_watched(self.current_inspected_folder)
+        folders_listbox_index = watched_folders.index(self.current_inspected_folder)
+        watched_folders.remove(self.current_inspected_folder)
+        self.folders_listbox.delete(folders_listbox_index)
+        self.inspected_folder_name_label.config(text='')
+        self.remove_button.config(state=DISABLED)
 
 
 def refresh():
@@ -25,15 +81,7 @@ def clear_cloud():
     pass
 
 
-def display_download_or_delete_tip():
-    pass
-
-
 def open_about_window():
-    pass
-
-
-def open_gui_overview():
     pass
 
 
@@ -41,152 +89,195 @@ def open_send_feedback_window():
     pass
 
 
-def open_storage_details_window():
-    pass
-
-
-def get_version_number():
-    return '1.3.0'
-
-
-def switch_to_list_layout():
-    pass
-
-
-def switch_to_grid_layout():
-    pass
-
-
-def open_inspect_file_panel(file):
-    pass
+def inspect_file(file):
+    print(file)
 
 
 def main():
-    root = Tk()
-    root.title('CFS Manager v' + get_version_number())
-    root.option_add('*tearOff', False) #removes dashed line from top of all cascading menus
-    main_menu = Menu(root)
-    root.config(menu=main_menu)
+    MainWindow()
 
-    cloud_files_panel = Frame(root, width=400)
-    file_inspect_panel = Frame(root)
-    storage_space_bar = Frame(root, height=100)
 
-    #initialize main_menu elements
-    local_submenu = Menu(main_menu)
-    main_menu.add_cascade(label='Local', menu=local_submenu)
-    local_submenu.add_command(label='Upload...', command=upload)
-    local_submenu.add_command(label='Upload All', command=upload_all)
-    local_submenu.add_command(label='Manage Watched Folders...', command=open_manage_watched_folders_window)
+class MainWindow:
 
-    cloud_submenu = Menu(main_menu)
-    main_menu.add_cascade(label='Cloud', menu=cloud_submenu)
-    cloud_submenu.add_command(label='Refresh', command=refresh)
-    cloud_submenu.add_command(label='Clear', command=clear_cloud)
-    #cloud_submenu.add_separator()
-    #cloud_submenu.add_command(label='Download...', command=display_download_or_delete_tip)
-    #cloud_submenu.add_command(label='Delete...', command=display_download_or_delete_tip)
+    def __init__(self):
+        self.current_inspected_file = None
+        self.manage_watched_folders_window = None
 
-    about_submenu = Menu(main_menu)
-    main_menu.add_cascade(label='About', menu=about_submenu)
-    about_submenu.add_command(label='About...', command=open_about_window)
-    about_submenu.add_command(label='Docs...', command=integration.open_docs)
-    about_submenu.add_command(label='Github...', command=integration.open_github)
-    about_submenu.add_command(label='License...', command=integration.open_license)
+        root = Tk()
+        root.title('CFS Manager v' + integration.get_version_number())
+        root.option_add('*tearOff', False)  # removes dashed line from top of all cascading menus
+        main_menu = Menu(root)
+        root.config(menu=main_menu)
 
-    help_submenu = Menu(main_menu)
-    main_menu.add_cascade(label='Help', menu=help_submenu)
-    help_submenu.add_command(label='Send Question/Request to the Developers...', command=open_send_feedback_window)
+        cloud_files_panel = Frame(root, width=400)
+        file_inspect_panel = Frame(root)
+        storage_space_bar = Frame(root, height=100)
 
-    #initialize storage space bar
-    storage_space_bar.pack(side=BOTTOM, anchor=SW, fill=X)
+        # initialize main_menu elements
+        local_submenu = Menu(main_menu)
+        main_menu.add_cascade(label='Local', menu=local_submenu)
+        local_submenu.add_command(label='Upload...', command=upload)
+        local_submenu.add_command(label='Upload All', command=upload_all)
+        local_submenu.add_command(label='Manage Watched Folders...', command=open_manage_watched_folders_window)
 
-    ssg_width = 80
-    ssg_height = 17
-    storage_space_graphic = Canvas(storage_space_bar, width=ssg_width, height=ssg_height)
-    storage_space_graphic.pack(side=LEFT)
-    space_used = integration.get_space_used()
-    total_space = integration.get_total_space()
-    sur_width = ssg_width * (space_used / total_space)
-    print(sur_width)
-    border_rectangle = storage_space_graphic.create_rectangle(0, 0, ssg_width, ssg_height, fill='lightgrey')
-    space_used_rectangle = storage_space_graphic.create_rectangle(0, 0, sur_width, ssg_height, fill='green', outline='')
+        cloud_submenu = Menu(main_menu)
+        main_menu.add_cascade(label='Cloud', menu=cloud_submenu)
+        cloud_submenu.add_command(label='Refresh', command=refresh)
+        cloud_submenu.add_command(label='Clear', command=clear_cloud)
 
-    space_used_text = Label(storage_space_bar, text=str(space_used) + ' GB used out of ' + str(total_space))
-    space_used_text.pack(side=LEFT)
+        about_submenu = Menu(main_menu)
+        main_menu.add_cascade(label='About', menu=about_submenu)
+        about_submenu.add_command(label='About...', command=open_about_window)
+        about_submenu.add_command(label='Docs...', command=integration.open_docs)
+        about_submenu.add_command(label='Github...', command=integration.open_github)
+        about_submenu.add_command(label='License...', command=integration.open_license)
 
-    #storage_details_button = Button(storage_space_bar, text="Storage Details...", command=open_storage_details_window)
-    #storage_details_button.pack(side=LEFT)
+        help_submenu = Menu(main_menu)
+        main_menu.add_cascade(label='Help', menu=help_submenu)
+        help_submenu.add_command(label='Send Question/Request to the Developers...', command=open_send_feedback_window)
 
-    #initialize cloud files panel
-    cloud_files_panel.pack(side=LEFT, fill=BOTH)
-    cfp_top_bar = Frame(cloud_files_panel, height=100)
-    cfp_top_bar.pack(side=TOP, fill=X)
+        # initialize storage space bar
+        storage_space_bar.pack(side=BOTTOM, anchor=SW, fill=X)
 
-    my_cloud_label = Label(cfp_top_bar, text='My Cloud')
-    my_cloud_label.pack(side=LEFT)
+        ssg_width = 80
+        ssg_height = 17
+        storage_space_graphic = Canvas(storage_space_bar, width=ssg_width, height=ssg_height)
+        storage_space_graphic.pack(side=LEFT)
+        space_used = integration.get_space_used()
+        total_space = integration.get_total_space()
+        sur_width = ssg_width * (space_used / total_space)
+        print(sur_width)
+        storage_space_graphic.create_rectangle(0, 0, ssg_width, ssg_height, fill='lightgrey')
+        storage_space_graphic.create_rectangle(0, 0, sur_width, ssg_height, fill='green', outline='')
 
-    """
-    list_layout_img = PhotoImage(file=os.path.join(image_dir, 'list_layout.png'))
-    grid_layout_img = PhotoImage(file=os.path.join(image_dir, 'grid_layout.png'))
+        space_used_text = Label(storage_space_bar, text=str(space_used) + ' GB used out of ' + str(total_space))
+        space_used_text.pack(side=LEFT)
 
-    list_layout_button = Button(cfp_top_bar, image=list_layout_img, command=switch_to_list_layout, width=20, height=20)
-    grid_layout_button = Button(cfp_top_bar, image=grid_layout_img, command=switch_to_grid_layout, width=20, height=20)
-    list_layout_button.pack(side=RIGHT)
-    grid_layout_button.pack(side=RIGHT)
-    """
+        # initialize cloud files panel
+        cloud_files_panel.pack(side=LEFT, fill=BOTH)
+        cfp_top_bar = Frame(cloud_files_panel, height=100)
+        cfp_top_bar.pack(side=TOP, fill=X)
 
-    files_grid_frame = Frame(cloud_files_panel)
-    files_grid_frame.pack(fill=BOTH)
-    folder_image = PhotoImage(file=os.path.join(image_dir, 'sample_folder_image.png'))
+        my_cloud_label = Label(cfp_top_bar, text='My Cloud')
+        my_cloud_label.pack(side=LEFT)
 
-    """
-    sample_file_1 = Label(files_grid_frame, image=folder_image)
-    sample_file_2 = Label(files_grid_frame, image=folder_image)
-    sample_file_3 = Label(files_grid_frame, image=folder_image)
-    sample_file_4 = Label(files_grid_frame, image=folder_image)
-    sample_file_5 = Label(files_grid_frame, image=folder_image)
-    sample_file_6 = Label(files_grid_frame, image=folder_image)
-    sample_file_7 = Label(files_grid_frame, image=folder_image)
-    sample_file_8 = Label(files_grid_frame, image=folder_image)
-    sample_file_9 = Label(files_grid_frame, image=folder_image)
+        scrollbar = Scrollbar(cloud_files_panel)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        self.file_listbox = Listbox(cloud_files_panel, yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.file_listbox.yview)
+        self.file_listbox.pack(side=LEFT, fill=BOTH)
 
-    sample_file_1.grid(row=0, column=0)
-    sample_file_2.grid(row=0, column=1)
-    sample_file_3.grid(row=0, column=2)
-    sample_file_4.grid(row=1, column=0)
-    sample_file_5.grid(row=1, column=1)
-    sample_file_6.grid(row=1, column=2)
-    sample_file_7.grid(row=2, column=0)
-    sample_file_8.grid(row=2, column=1)
-    sample_file_9.grid(row=2, column=2)
-    """
+        width = 20
+        for file in managed_files:
+            width = max(width, len(file))
+            self.file_listbox.insert(END, file)
+        self.file_listbox.config(width=min(50, width + 3))
 
-    #initialize file inspect panel
-    file_inspect_panel.pack(side=RIGHT, fill=Y)
+        self.file_listbox.bind('<ButtonRelease-1>', self.find_and_inspect_file)
 
-    sample_file_inspect_image = Label(file_inspect_panel, image=folder_image)
-    sample_file_inspect_image.pack(side=TOP, anchor=N)
-    label_1 = Label(file_inspect_panel, text='nameOfFile.txt')
-    label_2 = Label(file_inspect_panel, text='(in the future, you will')
-    label_3 = Label(file_inspect_panel, text='select a file from the left')
-    label_4 = Label(file_inspect_panel, text='and it will display here.')
-    label_5 = Label(file_inspect_panel, text='(and this text will be the')
-    label_6 = Label(file_inspect_panel, text='(--file-inspect info))')
-    label_1.pack(side=TOP)
-    label_2.pack(side=TOP)
-    label_3.pack(side=TOP)
-    label_4.pack(side=TOP)
-    label_5.pack(side=TOP)
-    label_6.pack(side=TOP)
-    download_default_button = Button(file_inspect_panel, text='Download (Default)')
-    download_custom_button = Button(file_inspect_panel, text='Download (Custom)...')
-    delete_button = Button(file_inspect_panel, text='Delete')
-    download_default_button.pack(side=TOP)
-    download_custom_button.pack(side=TOP)
-    delete_button.pack(side=TOP)
+        # initialize file inspect panel
+        file_inspect_panel.pack(side=RIGHT, fill=Y)
 
-    root.mainloop()
+        self.no_file_selected_100x100_image = PhotoImage(file=os.path.join(image_dir, 'no_file_selected_100x100.png'))
+        self.folder_100x100_image = PhotoImage(file=os.path.join(image_dir, 'folder_100x100.png'))
+        self.document_100x100_image = PhotoImage(file=os.path.join(image_dir, 'document_100x100.png'))
+        self.image_100x100_image = PhotoImage(file=os.path.join(image_dir, 'image_100x100.png'))
+
+        self.file_inspect_image_label = Label(file_inspect_panel, image=self.no_file_selected_100x100_image)
+        self.file_inspect_image_label.pack(side=TOP, anchor=N)
+        self.file_name_label = Label(file_inspect_panel, text='Name: ')
+        self.file_size_label = Label(file_inspect_panel, text='Size: ')
+        self.date_uploaded_label = Label(file_inspect_panel, text='Uploaded: ')
+        self.storage_provider_label = Label(file_inspect_panel, text='System type: ')
+        self.file_name_label.pack(side=TOP)
+        self.file_size_label.pack(side=TOP)
+        self.date_uploaded_label.pack(side=TOP)
+        self.storage_provider_label.pack(side=TOP)
+        Label(file_inspect_panel, text='- - - - - - - - - - - - - - - - - - - - - - - -').pack(side=TOP)
+        self.choose_download_folder_label = Label(file_inspect_panel, text='Choose destination:')
+        self.choose_download_folder_label.pack(side=TOP)
+        download_destination_frame = Frame(file_inspect_panel)
+        download_destination_frame.pack(side=TOP)
+        self.download_destination_entry = Entry(download_destination_frame)
+        self.download_destination_entry.insert(0, default_download_destination)
+        self.download_destination_entry.config(width=len(default_download_destination))
+        self.download_destination_entry.config(state=DISABLED)
+        self.download_destination_entry.pack(side=LEFT, fill=X)
+        self.choose_download_destination_button = Button(download_destination_frame, text='...',
+                                                         command=self.choose_download_destination)
+        self.choose_download_destination_button.config(state=DISABLED)
+        self.choose_download_destination_button.pack(side=RIGHT)
+        self.download_button = Button(file_inspect_panel, text='Download', command=self.download)
+        self.download_button.config(state=DISABLED)
+        self.download_button.pack(side=TOP)
+        Label(file_inspect_panel, text='- - - - - - - - - - - - - - - - - - - - - - - -').pack(side=TOP)
+        self.delete_button = Button(file_inspect_panel, text='Delete', command=self.delete)
+        self.delete_button.config(state=DISABLED)
+        self.delete_button.pack(side=TOP)
+
+        root.mainloop()
+
+    def find_and_inspect_file(self, event_data):
+        self.current_inspected_file = self.file_listbox.get(self.file_listbox.curselection()[0])
+        self.set_file_inspect_panel()
+
+    def choose_download_destination(self):
+        replacement_filepath = filedialog.askdirectory()
+        if replacement_filepath:
+            self.download_destination_entry.delete(0, END)
+            self.download_destination_entry.insert(0, replacement_filepath)
+
+    def download(self):
+        filepath = self.download_destination_entry.get()
+        viable = self.check_filepath_viability(filepath)
+        if viable:
+            integration.download(self.current_inspected_file, filepath)
+            print('downloaded')
+        else:
+            print('invalid filepath error')
+            exit()
+
+    def delete(self):
+        integration.delete(self.current_inspected_file)
+        index_in_file_listbox = managed_files.index(self.current_inspected_file)
+        managed_files.remove(self.current_inspected_file)
+        self.file_listbox.delete(index_in_file_listbox)
+        self.current_inspected_file = None
+        self.set_file_inspect_panel()
+        print('deleted')
+
+    def set_file_inspect_panel(self):
+        file = self.current_inspected_file
+        if not file:
+            self.file_inspect_image_label.config(image=self.no_file_selected_100x100_image)
+            self.file_name_label.config(text='Name: ')
+            self.file_size_label.config(text='Size: ')
+            self.date_uploaded_label.config(text='Uploaded: ')
+            self.storage_provider_label.config(text='Storage provider: ')
+            self.choose_download_destination_button.config(state=DISABLED)
+            self.download_button.config(state=DISABLED)
+            self.delete_button.config(state=DISABLED)
+            self.download_destination_entry.config(state=DISABLED)
+        else:
+            file_ext = file[-4:-1] + file[-1]
+            if file_ext == '.zip':
+                self.file_inspect_image_label.config(image=self.folder_100x100_image)
+            elif file_ext in ['.jpg', 'jpeg', '.png', '.gif']:
+                self.file_inspect_image_label.config(image=self.image_100x100_image)
+            else:
+                self.file_inspect_image_label.config(image=self.document_100x100_image)
+            file_info = integration.get_file_info(file)
+            self.file_name_label.config(text='Name: ' + file_info['filename'])
+            self.file_size_label.config(text='Size: ' + file_info['size'] + ' bytes')
+            self.date_uploaded_label.config(text='Uploaded: ' + file_info['date'])
+            self.storage_provider_label.config(text='Storage provider: ' + file_info['system type'])
+            self.choose_download_destination_button.config(state=NORMAL)
+            self.download_button.config(state=NORMAL)
+            self.delete_button.config(state=NORMAL)
+            self.download_destination_entry.config(state=NORMAL)
+
+    def check_filepath_viability(self, filepath):
+        return True
 
 
 if __name__ == '__main__':
